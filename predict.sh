2f6b78d1ae6d02a4d1f3f69afb1e91c2feeee209
zhaohongfei <iioSnail@163.com>
2023-06-20 09:30:41 +0800

    commit

--- predict.sh
+++ predict.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-REPO_PATH=/home/ljh/GEC/SCOPE
-BERT_PATH=/home/ljh/GEC/SCOPE/FPT
-DATA_DIR=$REPO_PATH/data
-export PYTHONPATH="$PYTHONPATH:$REPO_PATH"
-
-ckpt_path=outputs/bs32epoch30/checkpoint/epoch=25-df=80.6798-cf=78.3542.ckpt
+ckpt_path=outputs/checkpoint/epoch=29-df=80.2158-cf=78.5971.ckpt
 
 OUTPUT_DIR=outputs/predict
 mkdir -p $OUTPUT_DIR
 
-CUDA_VISIBLE_DEVICES=1  python -u finetune/predict.py \
-  --bert_path $BERT_PATH \
+python -u finetune/predict.py \
+  --bert_path FPT \
   --ckpt_path $ckpt_path \
-  --data_dir $DATA_DIR \
+  --data_dir data \
   --save_path $OUTPUT_DIR \
-  --label_file $DATA_DIR/test.sighan15.lbl.tsv \
+  --label_file data/test.sighan15.lbl.tsv \
   --gpus=0,
 
 
